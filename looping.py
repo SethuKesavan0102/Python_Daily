4b7f1c9d2e719b0211fe076ff259af1312a9b555
Hariharanvip <hariharan1862001@gmail.com>
2024-09-06 16:02:02 +0530

    while loop

--- looping.py
+++ looping.py
@@ -1,3 +1,19 @@
-a=5
+'''a=5
 for i in range(a,10,1):
-    print(i ,str."hello")
+    print(i)
+    
+b=5
+while(b<10):
+    print(b)
+    b=b+1'''
+
+a=int(input("Enter a value"))
+b=int(input("Enter b value"))
+c=int(input("Enter end value"))
+
+'''while(a<c):
+    print(a,"x",b,"=",a*b)
+    a+=1'''
+for i in range(a,c,1):
+    print(i,"x",b,"=",i*b)
+
