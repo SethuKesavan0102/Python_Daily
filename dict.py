acd17739be36bf8f9300794579283bd8b8243261
Hariharanvip <hariharan1862001@gmail.com>
2024-09-24 10:57:07 +0530

    dict

--- dict.py
+++ dict.py
@@ -18,12 +18,12 @@ print(d)'''
 
 #update function:
 
-d.update({'reg_no':155}) #we can update only one key values
+'''d.update({'reg_no':155}) #we can update only one key values
 #print(d)
 
 c={'qualification':'b.com'} #we can update more than one key values
 d.update(c)
-print(d)
+print(d)'''
 
 #delete
 
@@ -38,3 +38,80 @@ print(d)
 #del d
 #del d['name']
 #print(d)
+
+
+    #using for loop
+'''for i in d:
+    print(i)''' #default is key 
+
+'''for i in d:
+    print(d[i])     #to get value'''
+
+'''for i in d.items():    #you can use d.value(),d.keys(),d.items() to get direct element
+    print(i)'''
+
+'''for i in d.items():
+    x,y=i           #it assign key =x,values =y 
+    print(x,y)'''
+
+    #copy()
+'''a=d.copy()
+print(a)'''
+
+    #get()
+'''a=d.get("age")
+print(a)'''
+
+    #setdefault()
+'''b=d.setdefault('city','madurai')
+print(d)'''
+
+    #fromkeys()
+'''x=('address','state')
+y='tamilnadu'
+z=d.fromkeys(x,y)
+print(z)'''
+
+    #add key:values in dynamic method
+
+'''a=input("Enter the keys :")
+b=input("Enter the values :")
+#first method
+d[a]=b
+print(d)
+#second method
+c={a:b}
+d.update(c)
+print(d)'''
+
+    #nested dictionary
+a={'key':{'key':'value'}}
+
+a={'a1':{'Name':'raja','age':20,'city':'Madurai'},
+   'a2':{'Name':'ragu','age':21,'city':'Madurai'},
+   'a3':{'Name':'ravi','age':22,'city':'Madurai'}
+   }
+#print(a['a1']['Name'])
+
+
+#accessing values in dictionary
+
+'''for i,b in a.items():
+    print(i)
+    for j in b:
+        print(j,':',b[j])'''
+
+
+
+
+for i in a.items():
+    x,y=i
+    print(x)
+    for j in y:
+        print(j,':',y[j])
+
+
+    
+
+
+
